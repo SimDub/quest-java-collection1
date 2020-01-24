5679430b3fdca659eb648a572a190c0af9c8ca99
Simon DUBOELLE <duboelle.s@gmail.com>
2020-01-24 14:23:47 +0100

    Thanos win

--- Thanos.java
+++ Thanos.java
@@ -4,24 +4,36 @@ public class Thanos {
 
     public static void main(String[] args) {
 
-        // TODO 1 : Create an empty heroes list
+        Hero widow = new Hero ("Black Widow", 34);
+        Hero captain = new Hero ("Captain America", 100);
+        Hero vision = new Hero ("Vision", 3);
+        Hero iron = new Hero ("Iron Man", 48);
+        Hero scarlet = new Hero ("Scarlet Witch", 29);
+        Hero thor = new Hero ("Thor", 1500);
+        Hero peter = new Hero("Spider-Man", 18);
+        Hero hulk = new Hero ("Hulk", 49);
+        Hero doc = new Hero ("Doctor Strange", 42);
 
-        // TODO 2 : Add those heroes to the list
-        // name: Black Widow, age: 34
-        // name: Captain America, age: 100
-        // name: Vision, age: 3
-        // name: Iron Man, age: 48
-        // name: Scarlet Witch, age: 29
-        // name: Thor, age: 1500
-        // name: Hulk, age: 49
-        // name: Doctor Strange, age: 42
+        ArrayList<Hero> heroes = new ArrayList<>();
 
-        // TODO 3 : It's Thor birthday, now he's 1501
+        heroes.add(widow);
+        heroes.add(captain);
+        heroes.add(vision);
+        heroes.add(iron);
+        heroes.add(scarlet);
+        heroes.add(thor);
+        heroes.add(peter);
+        heroes.add(hulk);
+        heroes.add(doc);
 
-        // TODO 4 : Shuffle the heroes list
+        thor.setAge(1501);
 
-        // TODO 5 : Keep only the half of the list
+        Collections.shuffle(heroes);
 
-        // TODO 6 : Loop throught the list and display the name of the remaining heroes
+        List<Hero> leftAlive = heroes.subList(0, (heroes.size()/2));
+
+        for (Hero alive : leftAlive ) {
+            System.out.println(alive.getName());
+        }
     }
 }
